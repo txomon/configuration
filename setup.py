fa26a4d0731796de6c2f3242c6e853063aa06e4a
Javier Domingo Cansino <javierdo1@gmail.com>
2017-01-15 18:14:03 +0100

    Package is more or less ready for publication, need to do all the documentation and so

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from setuptools import setup
+
+setup(
+    name='configuration',
+    version='0.0.1a',
+    description='Configuration engine',
+    author='Javier Domingo Cansino',
+    author_email='javierdo1@gmail.com',
+    py_modules=['configuration'],
+    include_package_data=True,
+    zip_safe=False,
+)
